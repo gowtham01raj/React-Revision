a7e2f795b2f853c476b63dd8c5458acb7b122d48
gowtham01raj <gowthamraj.ts@outlook.com>
2025-06-20 14:39:17 +0530

    last one

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "this is the h1 tag"),
-    React.createElement("h2", {}, "this is the 2nd tag"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "this is the h1 tag"),
-    React.createElement("h2", {}, "this is the 2nd tag"),
-  ]),
-]);
+const Spam = <span>This is Not a heading 3</span>;
+const data = api.getData();
+const Heading = () => {
+  return (
+    <h1 id="h1" key="h1">
+      "this is the 1st Heading"
+      {Spam}
+    </h1>
+  );
+};
+
+const heading2 = 10;
+//JSX
+
+const Parent = () => (
+  <div id="parent" key="parent">
+    {Heading()}
+    {heading2}
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<Parent />);
